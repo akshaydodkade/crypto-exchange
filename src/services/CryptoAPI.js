9a603e294c0236b6f8cb0ac155670c5cc5eea63c
Akshay Dodkade <akshaydodkade@gmail.com>
2022-01-18 00:39:02 +0530

    api - fixed api key issue

--- src/services/CryptoAPI.js
+++ src/services/CryptoAPI.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const cryptoHeaders = {
   'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-  'x-rapidapi-key': 'c32823898fmshb13025325b1f43ap1c0a31jsnda3efa886ac3'
+  'x-rapidapi-key': 'e1aaf4632dmsha64eb961107192bp17bb1djsn8304bbc06ee6'
 }
 const baseUrl = 'https://coinranking1.p.rapidapi.com';
 
